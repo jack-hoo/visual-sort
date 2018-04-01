efbf77c7247dcbed5f1f77b59e05817657c5ade1
shugan <shugan@2dfire.com>
2018-04-01 22:24:47 +0800

    添加冒泡排序

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,109 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="main">
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <div class="card">
+          <div class="graph">
+            <ul>
+              <li v-for="(item,index) in array" :key="index"
+                  v-bind:style="{ height: item*5 + 'px' }">{{item}}
+              </li>
+              <!--<li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li >1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>
+              <li>1</li>-->
+            </ul>
+          </div>
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="card">
+          afeaw
+        </div>
+      </el-col>
+    </el-row>
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <div class="card">
+          as
+        </div>
+      </el-col>
+      <el-col :span="12">
+        <div class="card">
+          af
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        array: [15, 8, 9, 7, 55, 2, 44, 66, 3, 26]
+      }
+    },
+    methods: {
+      sort() {
+        for (let i = 0; i < this.array.length - 1; i++) {
+          for (let j = 0; j < this.array.length - 1 - i; j++) {
+            if (this.array[j] > this.array[j + 1]) {
+              let k = j
+              let temp = this.array[k];
+              this.array[k] = this.array[k + 1];
+              this.array[k + 1] = temp;
+              this.array.reverse().reverse()
+              console.log("joiojio")
+            }
+
+          }
+        }
+      }
+    },
+    mounted() {
+      this.sort()
+      /*setInterval(()=>{
+        this.array[0] = this.array[0] +10
+        this.array.reverse().reverse()
+      },1000)*/
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .main {
+
+  }
+
+  .card {
+    background: antiquewhite;
+    height: 360px;
+    margin-top: 10px;
+  }
+
+  .graph ul {
+    display: inline-flex;
+    position: absolute;
+    bottom: 10px;
+    align-items: flex-end;
+    /*transform:rotate(180deg);*/
+  }
+
+  .graph ul > li {
+
+    list-style: none;
+    width: 20px;
+    height: 60px;
+    background-color: orangered;
+    margin-left: 3px;
+    margin-right: 3px;
+  }
 </style>
